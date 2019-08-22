344d713dc815815b90f09e8a6c539ef1311ab9b4
俣江幸治 <kmatae@Matae-Mac.local>
2019-08-22 16:22:56 +0900

    初期コード

---
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -0,0 +1,35 @@
+import { LightningElement, track, wire } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountList.getAccounts';
+
+/**
+ * カラム定義
+ */
+const columns = [
+    { label: '取引先名', fieldName: 'Name' },
+    { label: '都道府県', fieldName: 'BillingState' }
+
+];
+
+export default class AccountList extends LightningElement {
+    /**
+     * カラム定義
+     */
+    @track columns = columns;
+
+    /**
+     * 取引先データ
+     */
+    @track record;
+
+    /**
+     * 取引先データ取得
+     */
+    @wire(getAccounts)
+    wiredGetAccounts({error, data}) {
+        if (data) {
+            this.record = data;
+        } else {
+            this.record = undefined;
+        }
+    }
+}
